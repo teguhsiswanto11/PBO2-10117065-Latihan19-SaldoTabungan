f54ddcbe30fd494dc97a42fac1645622d71aa15b
Teguh Siswanto <tsiswanto73@gmail.com>
2018-09-30 00:46:58 +0700

    first commit pada project ini - PBO Latihan Tugas Pertemuan 2

---
+++ src/pbo2/pkg10117065/latihan19/saldotabungan/PBO210117065Latihan19SaldoTabungan.java
@@ -0,0 +1,35 @@
+package pbo2.pkg10117065.latihan19.saldotabungan;
+
+/**
+ * @author 
+ * NAMA     : Teguh Siswanto
+ * KELAS    : IF-2
+ * NIM      : 10117065
+ * Deskripsi Program : 
+ * 
+ */
+public class PBO210117065Latihan19SaldoTabungan {
+
+    public static void main(String[] args) {
+//        deklarasi variabel
+        int saldoAwal;
+        int bunga; // persen bunga
+        int jbunga; // jumlah bunga
+        int lamaBulan;
+//        variabel yang diberi value
+        saldoAwal = 2500000;
+        bunga = 15;
+        lamaBulan = 6;
+        
+//        proses looping selama 6x
+        for (int i = 1; i <= lamaBulan; i++) {
+            //rumus 
+            jbunga = saldoAwal*bunga/100;
+            saldoAwal += jbunga;
+            //menampilkan dengan format tertentu
+            String mataUang = String.format("Rp.%,3d", saldoAwal).replaceAll(",", ".");
+            System.out.printf("Saldo di bulan ke-"+i+" %s%n",mataUang);
+        }
+    }
+    
+}
